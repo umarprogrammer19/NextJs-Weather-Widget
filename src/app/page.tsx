b1f8b6b13e156166a50ac048417942c4bdfd7882
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-10-09 23:42:34 +0500

    Added Packeges

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import React from "react";
+import WeatherWidget from "@/components/weather-widget";
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>Hello World</h1>
+      <WeatherWidget />
+    </>
   );
 }
---
+++ src/components/weather-widget.tsx
@@ -0,0 +1,17 @@
+"use client";
+import {
+    Card,
+    CardHeader,
+    CardTitle,
+    CardDescription,
+    CardContent,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { CloudIcon, MapPinIcon, ThermometerIcon } from "lucide-react";
+
+export default function WeatherWidget() {
+    return (
+        <h1>Weather Widget</h1>
+    )
+}
