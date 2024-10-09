ed1f0572d03c02ea9089b643726e46c2b83ef30a
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-10-09 23:47:37 +0500

    Setted State

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import WeatherWidget from "@/components/weather-widget";
 export default function Home() {
   return (
     <>
-      <h1>Hello World</h1>
       <WeatherWidget />
     </>
   );
--- src/components/weather-widget.tsx
+++ src/components/weather-widget.tsx
@@ -9,8 +9,20 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { CloudIcon, MapPinIcon, ThermometerIcon } from "lucide-react";
+import { useState } from "react";
+
+interface WeatherData {
+    temperature: number;
+    description: string;
+    location: string;
+    unit: string;
+}
 
 export default function WeatherWidget() {
+    const [location, setLocation] = useState<string>("");
+    const [weather, setWeather] = useState<WeatherData | null>(null);
+    const [error, setError] = useState<string | null>(null);
+    const [loadind, setLoading] = useState<boolean>(false);
     return (
         <h1>Weather Widget</h1>
     )
