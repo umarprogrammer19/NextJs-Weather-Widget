28e8eb09b44c5ff1721a0f5073727b71c3ae5aa2
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2024-10-10 01:13:35 +0500

    Fixed Production Error

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -2,10 +2,6 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  customProp?: string;
-}
-
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
     return (
